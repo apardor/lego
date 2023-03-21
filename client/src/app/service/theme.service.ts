685899671a18b05599c8449ec6a53d0bf520d27d
apardor <apardor@gmail.com>
2023-03-21 14:41:37 +0100

    first commit client+server

--- client/src/app/service/theme.service.ts
+++ client/src/app/service/theme.service.ts
@@ -9,6 +9,6 @@ export class ThemeService {
   constructor(private http: HttpClient) {}
 
   getThemeData(){
-    return this.http.get('https://rebrickable.com/api/v3/lego/themes/?key=b32f1aff042501520288add316a77ad3');
+    return this.http.get('');
   }
 }
