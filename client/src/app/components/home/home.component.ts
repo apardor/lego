20d5d86b8e4bb59b638d19155e1f51aa841e7886
apardor <apardor@gmail.com>
2023-03-23 16:38:22 +0100

    added p5.js and canvas on home

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,33 @@ export class HomeComponent implements OnInit {
 
   constructor(){}
 
+
   ngOnInit(): void {
+    let sketch = function(p: any) {
+      let img:any;
+      let clickX;
+      let clickY;
 
+      p.preload = function(){
+        img = p.loadImage('../../assets/minifigure-250.png')
+      }
+
+      p.setup = function() {
+        p.createCanvas(p.windowWidth,p.windowHeight );
+        p.background(255);
+        
+      };
+  
+      p.draw = function() {
+        clickX = p.mouseX - 100;
+        clickY = p.mouseY - 112;
+        if(p.mouseIsPressed){
+          p.image(img,clickX,clickY)
+        }
+      };
+
+ 
+    }
+    new p5(sketch);
   }
-
 }
