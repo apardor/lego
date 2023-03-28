cb405147861a2dd4341e5c0f5e02378e60c3e85b
apardor <apardor@gmail.com>
2023-03-28 15:12:59 +0200

    added OnDestroy to flush drawing and added active class to menu

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,44 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as p5 from 'p5';
+
+let sketch: p5;
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+
+
+export class HomeComponent implements OnInit, OnDestroy {
 
   myData: any;
 
   constructor(){}
 
+  ngOnInit() {
+    sketch = new p5((p) => {
 
-  ngOnInit(): void {
-    let sketch = function(p: any) {
       let img:any;
       let clickX;
       let clickY;
-
       p.preload = function(){
         img = p.loadImage('../../assets/minifigure-250.png')
-      }
+       }
 
-      p.setup = function() {
+      p.setup = () => {
         p.createCanvas(p.windowWidth,p.windowHeight );
         p.background(255);
-        
       };
-  
-      p.draw = function() {
-        clickX = p.mouseX - 100;
-        clickY = p.mouseY - 112;
-        if(p.mouseIsPressed){
-          p.image(img,clickX,clickY)
-        }
+      
+      p.draw = () => {
+        p.draw = function() {
+               clickX = p.mouseX - 100;
+               clickY = p.mouseY - 112;
+               if(p.mouseIsPressed){
+                 p.image(img,clickX,clickY)
+               }
+             };     
       };
-
- 
-    }
-    new p5(sketch);
+    });
   }
-}
+
+  ngOnDestroy() {
+    sketch.remove();
+  }
+  
+}    
+  
+
+
+  
+
+  
--- server/config.ts
+++ server/config.ts
@@ -5,8 +5,9 @@ const API_KEY = process.env.API_KEY || '';
 const THEME_URL = process.env.THEME_URL || '';
 const SETS_URL = process.env.SETS_URL || '';
 const THEME_PAGE_SIZE = process.env.THEME_PAGE_SIZE || '';
+const SET_PAGE_SIZE = process.env.SET_PAGE_SIZE || '';
 const THEME_URL_API = `${THEME_URL}?key=${API_KEY}&page_size=${THEME_PAGE_SIZE}`;
-const SETS_URL_API = `${SETS_URL}?key=${API_KEY}`
+const SETS_URL_API = `${SETS_URL}?key=${API_KEY}&page_size=${SET_PAGE_SIZE}`
 
 export const config = {
         url: THEME_URL_API,
